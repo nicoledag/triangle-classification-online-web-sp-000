25d943f6cb1ead265f78d5a6656c5b3d660d29fe
Nicole D <nnardella@gmail.com>
2018-11-30 02:21:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind(x, y, z)
+      binding.pry
+    if x == y && y == z
+      :equilateral
+
+    else
+      puts nothing
+    end
+
+
+  end
+
 end
